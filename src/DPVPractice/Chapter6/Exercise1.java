a9803dba16fea86da2872a9b273eedb9522450cc
Cao, Yirui <yrcao95@gatech.edu>
2022-09-04 00:32:38 -0700

    DPV Chapter Ex1

--- src/DPVPractice/Chapter6/Exercise1.java
+++ src/DPVPractice/Chapter6/Exercise1.java
@@ -13,4 +13,26 @@ For the preceding example, the answer would be 10,−5, 40, 10, with a sum of 55
 exactly at position j .)
  */
 public class Exercise1 {
+    public static int solution(int[] arr) {
+        int[] dp = new int[arr.length];
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) {
+                dp[i] = arr[i];
+            } else {
+                if (dp[i - 1] >= 0) {
+                    dp[i] = dp[i - 1] + arr[i];
+                } else {
+                    dp[i] = arr[i];
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(solution(arr));
+    }
 }
